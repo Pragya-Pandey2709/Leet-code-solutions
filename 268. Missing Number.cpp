feb616aa59e2b5438718957377e27c867e389fe2
Pragya Pandey <86335220+Pragya-Pandey2709@users.noreply.github.com>
2023-01-29 23:27:38 +0530

    Create 268. Missing Number.cpp

---
+++ 268. Missing Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int result = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            result ^= nums[i];
+            result ^= i;
+        }
+        result ^= nums.size();
+        return result;
+    }
+};
